57895b750995ecf3c3000538fbe1ab48dd275c16
Jim ETEVENARD <jetevenard@oxiane.com>
2023-01-26 18:51:41 +0100

    Test : fail scenarios
    TU du pauvre

--- src/js/test.js
+++ src/js/test.js
@@ -1,12 +1,22 @@
 const sendWeatherRequest = require('./meteo/meteo');
 
 
-// Saint-Mandé (94) France
+// Saint-Mandé (94) - France
 const geoLoc = {
     latitude: 48.84,
     longitude: 2.42
 };
 
+function fail(){
+    throw new Error('Test failed !');
+}
+
 sendWeatherRequest(geoLoc).then(
-    (result) => console.log(result)
+    (result) => {
+        console.log(result);
+
+        if(result?.weathercode == undefined) fail();
+        if(!result?.time) fail();
+        if(result?.good == undefined) fail();
+    }
 );
